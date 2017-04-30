8661f0122532719d3e5cbaacbc236c00e2e8e64e
Kazemipour, Abbas <kazemipoura@janelia.hhmi.org>
2017-04-30 17:51:51 -0400

    added comments to the code and test simulations

--- FADE.m
+++ FADE.m
@@ -1,42 +1,46 @@
 function sys_fade = FADE( sys )
+
+%% An implementation of the FAst DEconvolution (FADE) Algorithm
+% Input: Structure called sys with the following fields:
+%        y: observed calcium traces where the first dimension is the number of ROI's and
+%        second dimension is time and
+%        p_norm and q_norm: Penalizes the ell_pq norm of spikes, default: p = q = 1
+%        lambda: regularization parameter, default: lambda = 0 (ML Estimation)
+%        Order: Autoregressive model order, defualt: Order = 2
+%        theta: AR parameters, default: estimated using Yule-Walker equations
+%        noise: estimate of the noise standard variation, default:
+%        estimate using power spectral density
+%        min_iters: minimum number of iterations, default: min_iters = 100
+%        num_iters: maximum number of iterations, default: num_iters = 200
+% Output: A structure containing the following fields
+%        spikes: the deconvolved spikes, post processing methods should be
+%        smoothed_traces: smoothed calcium traces
+%        used on the spikes in most cases
+%        ds: relative changes in the spikes        
+%%        
 sys = default_opts_fade(sys);
 [p,T] = size(sys.y);
 
 b = sys.b0;
 s = sys.s0;
-% objective = zeros(1,sys.num_iters);
 ds = zeros(1,sys.num_iters);
 iter = 1;
 ds(1) = 1;
     while iter < sys.num_iters && ( ds(iter)>0.005 || iter<sys.min_iters)
-        [Lp,Ln] = calc_L(sys,s,b);
-        Pp = calc_penalty(sys,s);
-%         X = filter(1,sys.theta,s,[],2);
-%         bnew = min(X,[],2);
-        
-%         bnew = max(sum(sys.y-X,2)/T,0);
-
-%         bnew = sum(sys.y,2)./sum(X+b,2);
-        snew = Ln./(Lp+sys.lambda*Pp).*s;
-
-
-%         objective(iter) = norm(diag(1./sys.noise.^2)*(sys.y-X),'fro')^2 + sys.lambda*sum(s(:));
+        [Lp,Ln] = calc_L(sys,s,b); % positive and negative log-likelihood terms
+        Pp = calc_penalty(sys,s); % Penalty terms
+        snew = Ln./(Lp+sys.lambda*Pp).*s; % multiplicative updates
         ds(iter+1) = max(abs((sum(s,2)-sum(snew,2))./sum(s,2)));
-        
         s = snew;
-%         b = repmat(bnew,1,T);
         iter = iter + 1;
     end
 X = filter(1,sys.theta,s,[],2);
 
 sys_fade = sys;
-% sys_fade.b = b;
 sys_fade.spikes = s;
 sys_fade.smoothed_traces = X + sys.baseline;
 sys_fade.theta = sys.theta;
-% sys_fade.objective = objective(1:iter-1);
 sys_fade.ds = ds(1:iter-1);
-
 end
 
 function [Lp,Ln] = calc_L(sys,s,b)
@@ -50,17 +54,6 @@ function Pp = calc_penalty(sys,s)
     A2 = repmat(sum(s.^sys.p_norm,2).^(sys.q_norm/sys.p_norm-1),1,sys.T);
     A3 = s.^(sys.p_norm-1);
     Pp = A1*A2.*A3;
-%     switch sys.pen_norm
-%         case 'l1'
-%             Pp = ones(size(s));
-%         case 'l2_sq'
-%             Pp = 2*s;
-%         case 'l2'
-%             T = size(s,2);
-%             ss = repmat(sqrt(sum(s.^2,2)),1,T);
-%             Pp = 2*s./ss;
-%     end
-
 end
 
 
--- fade_test.m
+++ fade_test.m
@@ -1,19 +1,20 @@
 clear;clc; close all;
-p = 100; T = 10000; s = 100;
-w = zeros(p,T);
+%% Generate data
+p = 100;   % 100 ROI's
+T = 10000; % number of frames
+s = 100;   % sparsity 
+w = zeros(p,T); %spikes
 for i=1:p
 w(i,randsample(T,s)) = 1;
 end
-theta = [1 -.95];
-b = 2;
-X = filter(1,theta,w,[],2);
-sys.y = X+0.1*randn(size(w))+b;
+theta = [1 -.95]; % AR parameters
+b = 2; % baseline
+X = filter(1,theta,w,[],2); %clean calcium traces
+sys.y = X+0.1*randn(size(w))+b; %Noisy calcium traces
 %%
-% sys.Order = 2;
-sys.lambda = 1000;
-% sys.pen_norm = 'l1';
-sys.p_norm = 1;
-sys.q_norm = 1;
+sys.lambda = 1000; %regularization parameter
+sys.p_norm = 1;    % p norm in ell_pq penalty on spikes
+sys.q_norm = 1;    % q norm in ell_pq penalty on spikes
 message = sprintf('Penalizing ell_%d,%d norm of spikes',sys.p_norm,sys.q_norm);
 disp(message)
 
@@ -26,12 +27,13 @@ figure(1);
 f1 = subplot(3,1,1); plot(sys.y(index,:)); title('Observed')
 f2 = subplot(3,1,2); plot(sys_fade.smoothed_traces(index,:)); title('Ground-Truth')
 hold on; stem(w(index,:),'marker','none');
-f3 = subplot(3,1,3); stem(sys_fade.spikes(index,:),'marker','none'); title('Recovered')
+f3 = subplot(3,1,3); stem(sys_fade.spikes(index,:),'marker','none'); title('Recovered Spikes: No post processing done')
 linkaxes([f1 f2 f3],'x')
 %%%%%%%
 % figure(2);
 % plot(sys_fcss.objective(2:end));
 figure;
-plot(sys_fade.ds(2:end));
+plot(100*sys_fade.ds(2:end)); title('Relative Changes in estimated spikes')
+ylabel('Percent'), xlabel('Iteration')
 % sys_fcss.b
 %%%%%%
