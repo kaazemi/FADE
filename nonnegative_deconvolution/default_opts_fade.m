4f3e08ad8d70b1e9c913f3c6a803774c065f02c9
Kazemipour, Abbas <kazemipoura@janelia.hhmi.org>
2017-06-03 16:26:23 -0400

    added more comments to default opts
    made estimation of time constant robust to nans

--- nonnegative_deconvolution/default_opts_fade.m
+++ nonnegative_deconvolution/default_opts_fade.m
@@ -1,35 +1,47 @@
 function [ sys ] = default_opts_fade( sys )
 T = size(sys.y,2);
+    % Order of the Autoregressive Model
     if ~isfield(sys,'Order'); sys.Order = 2;                        end
+    % Minimum number of multiplicative updates
     if ~isfield(sys,'min_iters'); sys.min_iters = 100;              end
+    % Maximum number of multiplicative updates
     if ~isfield(sys,'num_iters'); sys.num_iters = 200;              end
+    % Variance of noise
     if ~isfield(sys,'noise'); sys.noise = estimate_noise(sys.y);    end
+    % Regularization parameter
     if ~isfield(sys,'lambda'); sys.lambda = 0;                      end
-%     if ~isfield(sys,'pen_norm'); sys.pen_norm = 'l2_sq';            end
+	% (p,q)-pair in the l_pq regularization of spikes
     if ~isfield(sys,'p_norm'); sys.p_norm = 1;            end
     if ~isfield(sys,'q_norm'); sys.q_norm = 1;            end
 if sys.lambda > 0
-     message = sprintf('Penalizing ell_%0.1f,%0.1f norm of spikes',sys.p_norm,sys.q_norm);
-else message = 'Warning: Lambda set to 0, solving the ML problem';
+    message = sprintf('Penalizing ell_%0.1f,%0.1f norm of spikes',sys.p_norm,sys.q_norm);
+else
+    message = 'Warning: Lambda set to 0, solving the ML problem';
 end
     disp(message)
+    % scale regularization parameter with respect to the length of data (from theory of lasso)
     sys.lambda = sys.lambda*sqrt(log(T)/T)*diag(sys.noise);
-    
+    % Estimate the baseline (it can also be set to be
     sys.baseline = estimate_base(sys.y,sys.noise);
+    % Subtract the baseline for deconvolution
     sys.y = sys.y - sys.baseline;
+    % treat everything under the baseline as noise and set it to zero
     sys.y(sys.y<0) = 0;
     sys.b0 = zeros(size(sys.y));
-%     
-%     sys.y = sys.y-repmat(min(sys.y,[],2),1,size(sys.y,2));
-%     sys.b0 = 0.1*ones(size(sys.y));
-    
-%     sys.b0 = zeros(size(sys.y));
-%     sys.b0 = repmat(min(sys.y,[],2),1,size(sys.y,2));
-
-    if ~isfield(sys,'theta'); yt = sys.y'; 
-        sys.theta = mean(aryule(yt(:,1:min(100,end)),sys.Order));   end
-        % ytvec = yt(:);
-        % sys.theta = aryule(ytvec(1:min(10000,end)),sys.Order);    end      
+    % estimate the time constant nd corresponding filter parameters
+    if isfield(sys,'tau')
+        sys.Order = 1;
+        sys.theta = [1 -exp(-1/sys.tau)];
+    else
+        if ~isfield(sys,'theta'); yt = sys.y'; 
+            if size(sys.y,1) == 1
+                sys.theta = nanmean(aryule(yt,sys.Order));
+            else
+            sys.theta = nanmean(aryule(yt(:,1:min(100,end)),sys.Order));   
+            end
+        end
+    sys.tau = 1/log(-1/sum(sys.theta(2:end)));
+    end
     
         sys.s0 = filter(sys.theta,1,sys.y,[],2); sys.s0(sys.s0<=0) = 1;
 %     sys.s0 = ones(size(sys.y));
@@ -54,5 +66,4 @@ mask = y < 4.5*noise;
 y(~mask) = nan;
 baseline = nanmean(y,2);
 baseline = repmat(baseline,1,T)+m;
-
 end
